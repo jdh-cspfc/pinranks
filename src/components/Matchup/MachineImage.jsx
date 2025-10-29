73217ba6953a524cb1695b388d40ca816b89441f
Jeff Hart <jeffdouglashart@gmail.com>
2025-10-28 20:28:23 -0700

    Fixed: Machine images on desktop not scaling with cards/browser size

--- src/components/Matchup/MachineImage.jsx
+++ src/components/Matchup/MachineImage.jsx
@@ -13,8 +13,8 @@ export default function MachineImage({ machine, name, imageUrl, imageState }) {
           </div>
         </div>
         {/* Desktop layout */}
-        <div className="hidden sm:absolute sm:inset-0 sm:flex sm:items-center sm:justify-center">
-          <div className="mx-auto mb-2 flex items-center justify-center w-full h-64 lg:h-80 xl:h-96">
+        <div className="hidden sm:absolute sm:inset-0 sm:flex sm:items-center sm:justify-center sm:pt-16">
+          <div className="mx-auto mb-2 flex items-center justify-center w-full h-full">
             <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500 dark:border-blue-400"></div>
           </div>
         </div>
@@ -43,12 +43,17 @@ export default function MachineImage({ machine, name, imageUrl, imageState }) {
         />
       </div>
       {/* Desktop layout */}
-      <div className="hidden sm:absolute sm:inset-0 sm:flex sm:items-center sm:justify-center">
+      <div className="hidden sm:absolute sm:inset-0 sm:flex sm:items-center sm:justify-center sm:pt-16">
         <img
           src={imageUrl}
           alt={name}
-          className="mx-auto mb-2 object-contain max-h-64 lg:h-80 xl:h-96 w-auto"
-          style={{ maxWidth: '100%' }}
+          className="mx-auto mb-2 object-contain w-auto"
+          style={{ 
+            maxHeight: '70%',
+            maxWidth: '100%',
+            height: 'auto',
+            width: 'auto'
+          }}
         />
       </div>
     </>
